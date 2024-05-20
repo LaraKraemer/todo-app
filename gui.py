3e64acaf3f8ab960038f7f6cbf1eb6198baddbeb
LaraKraemer <51157020+LaraKraemer@users.noreply.github.com>
2024-05-20 17:19:18 +0200

    Add directory for icon files

--- gui.py
+++ gui.py
@@ -7,15 +7,15 @@ sg.theme("DarkBlue5")
 label_clock = sg.Text("", key="clock")
 label = sg.Text("Type in a todo")
 input_box = sg.InputText(tooltip= "Enter a todo", key="todo")
-add_button = sg.Button(size=2, image_source="add.png",
+add_button = sg.Button(size=2, image_source="icons/add.png",
                        mouseover_colors="LightBlue1",
                        tooltip="Add a todoo item", key="Add")
 list_box = sg.Listbox(values=functions.get_todos(), key="todos",
                       enable_events=True, size=[45,10])
 
-edit_button = sg.Button(size=2, image_source="edit.png", tooltip="Edit a todo item",
+edit_button = sg.Button(size=2, image_source="icons/edit.png", tooltip="Edit a todo item",
                             mouseover_colors="LightBlue1", key="Edit")
-complete_button = sg.Button(size=2, image_source="complete.png", tooltip="Complete a todo item",
+complete_button = sg.Button(size=2, image_source="icons/complete.png", tooltip="Complete a todo item",
                             mouseover_colors="LightBlue1", key="Complete")
 exit_button = sg.Button("Exit")
 
