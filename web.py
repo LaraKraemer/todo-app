c5b17fbaf7a15adb45050a47c5ddffa7baec42a2
LaraKraemer <51157020+LaraKraemer@users.noreply.github.com>
2024-05-21 15:59:37 +0200

    Add the delete function

--- web.py
+++ web.py
@@ -3,6 +3,7 @@ import functions
 
 todos = functions.get_todos()
 
+# adding feature for new items
 def add_todo():
     todo= st.session_state["new_todo"] + "\n"
     todos.append(todo)
@@ -12,10 +13,15 @@ st.title("My Todo app")
 st.subheader("This is my todo app")
 st.text("This app helps you organize your daily tasks")
 
-for todo in todos:
-    st.checkbox(todo)
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    # deleting feature for completed items
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.experimental_rerun()
 
+# input box
 st.text_input(label="", placeholder="Add new Todo item...",
               on_change=add_todo, key="new_todo")
-
-st.session_state
